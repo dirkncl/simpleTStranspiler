e216d54772ca99794cb938c50aa35fb02102a528
Dirk Levinus Nicolaas <dirkncl@gmail.com>
2025-04-13 18:31:53 +0800

    Typescript: 5.9.0-dev

---
+++ typescript/deprecatedCompat/deprecate.js
@@ -0,0 +1,66 @@
+import { Debug, formatStringFromArgs, noop, Version, version, } from "./_namespaces/ts.js";
+
+export let enableDeprecationWarnings = true;
+
+export function setEnableDeprecationWarnings(value) {
+    enableDeprecationWarnings = value;
+}
+
+let typeScriptVersion;
+
+function getTypeScriptVersion() {
+    return typeScriptVersion !== null && typeScriptVersion !== void 0 ? typeScriptVersion : (typeScriptVersion = new Version(version));
+}
+
+function formatDeprecationMessage(name, error, errorAfter, since, message) {
+    let deprecationMessage = error ? "DeprecationError: " : "DeprecationWarning: ";
+    deprecationMessage += `'${name}' `;
+    deprecationMessage += since ? `has been deprecated since v${since}` : "is deprecated";
+    deprecationMessage += error ? " and can no longer be used." : errorAfter ? ` and will no longer be usable after v${errorAfter}.` : ".";
+    deprecationMessage += message ? ` ${formatStringFromArgs(message, [name])}` : "";
+    return deprecationMessage;
+}
+
+function createErrorDeprecation(name, errorAfter, since, message) {
+    const deprecationMessage = formatDeprecationMessage(name, /*error*/ true, errorAfter, since, message);
+    return () => {
+        throw new TypeError(deprecationMessage);
+    };
+}
+
+function createWarningDeprecation(name, errorAfter, since, message) {
+    let hasWrittenDeprecation = false;
+    return () => {
+        if (enableDeprecationWarnings && !hasWrittenDeprecation) {
+            Debug.log.warn(formatDeprecationMessage(name, /*error*/ false, errorAfter, since, message));
+            hasWrittenDeprecation = true;
+        }
+    };
+}
+
+export function createDeprecation(name, options = {}) {
+    var _a, _b;
+    const version = typeof options.typeScriptVersion === "string" ? new Version(options.typeScriptVersion) : (_a = options.typeScriptVersion) !== null && _a !== void 0 ? _a : getTypeScriptVersion();
+    const errorAfter = typeof options.errorAfter === "string" ? new Version(options.errorAfter) : options.errorAfter;
+    const warnAfter = typeof options.warnAfter === "string" ? new Version(options.warnAfter) : options.warnAfter;
+    const since = typeof options.since === "string" ? new Version(options.since) : (_b = options.since) !== null && _b !== void 0 ? _b : warnAfter;
+    const error = options.error || errorAfter && version.compareTo(errorAfter) >= 0;
+    const warn = !warnAfter || version.compareTo(warnAfter) >= 0;
+    return error ? createErrorDeprecation(name, errorAfter, since, options.message) :
+        warn ? createWarningDeprecation(name, errorAfter, since, options.message) :
+            noop;
+}
+
+function wrapFunction(deprecation, func) {
+    return function () {
+        deprecation();
+        // eslint-disable-next-line prefer-rest-params
+        return func.apply(this, arguments);
+    };
+}
+
+export function deprecate(func, options) {
+    var _a;
+    const deprecation = createDeprecation((_a = options === null || options === void 0 ? void 0 : options.name) !== null && _a !== void 0 ? _a : Debug.getFunctionName(func), options);
+    return wrapFunction(deprecation, func);
+}
---
+++ typescript/deprecatedCompat/deprecations.js
@@ -0,0 +1,52 @@
+import { hasProperty, } from "./_namespaces/ts.js";
+import { deprecate } from "./deprecate.js";
+
+/** @internal @knipignore */
+export function createOverload(name, overloads, binder, deprecations) {
+    Object.defineProperty(call, "name", Object.assign(Object.assign({}, Object.getOwnPropertyDescriptor(call, "name")), { value: name }));
+    if (deprecations) {
+        for (const key of Object.keys(deprecations)) {
+            const index = +key;
+            if (!isNaN(index) && hasProperty(overloads, `${index}`)) {
+                overloads[index] = deprecate(overloads[index], Object.assign(Object.assign({}, deprecations[index]), { name }));
+            }
+        }
+    }
+    const bind = createBinder(overloads, binder);
+    return call;
+    function call(...args) {
+        const index = bind(args);
+        const fn = index !== undefined ? overloads[index] : undefined;
+        if (typeof fn === "function") {
+            return fn(...args);
+        }
+        throw new TypeError("Invalid arguments");
+    }
+}
+
+function createBinder(overloads, binder) {
+    return args => {
+        for (let i = 0; hasProperty(overloads, `${i}`) && hasProperty(binder, `${i}`); i++) {
+            const fn = binder[i];
+            if (fn(args)) {
+                return i;
+            }
+        }
+    };
+}
+
+// NOTE: We only use this "builder" because we don't infer correctly when calling `createOverload` directly in < TS 4.7,
+//       but lib is currently at TS 4.4. We can switch to directly calling `createOverload` when we update LKG in main.
+/** @internal @knipignore */
+export function buildOverload(name) {
+    return {
+        overload: overloads => ({
+            bind: binder => ({
+                finish: () => createOverload(name, overloads, binder),
+                deprecate: deprecations => ({
+                    finish: () => createOverload(name, overloads, binder, deprecations),
+                }),
+            }),
+        }),
+    };
+}
